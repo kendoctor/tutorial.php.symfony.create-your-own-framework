d0a73598cbc6e4fe590c7316ef1754e66fbe7896
kendoctor <kendoctor@163.com>
2014-05-13 16:47:21 +0800

    index.php added

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Administrator
+ * Date: 14-5-13
+ * Time: 下午4:45
+ * To change this template use File | Settings | File Templates.
+ */
+
+$input = $_GET['name'];
+
+printf('Hello %s', $input);
